427754a0795b604fb2b81d335496df7ccbb87348
Tim Park <timfpark@gmail.com>
2018-11-19 08:26:02 -0800

    add jaeger endpoints

--- inputs.tf
+++ inputs.tf
@@ -3,23 +3,19 @@ variable "helm_install_timeout" {
 }
 
 variable "ingress_replica_count" {
-  type    = "string"
-  default = "3"
+  default = 3
 }
 
 variable "prometheus_enabled" {
-  type    = "string"
-  default = "true"
+  default = true
 }
 
 variable "ssl_enabled" {
-  type    = "string"
-  default = "false"
+  default = false
 }
 
 variable "ssl_enforced" {
-  type    = "string"
-  default = "false"
+  default = false
 }
 
 variable "ssl_cert_base64" {
@@ -30,4 +26,12 @@ variable "ssl_cert_base64" {
 variable "ssl_key_base64" {
   type    = "string"
   default = ""
+}
+
+variable "tracing_enabled" {
+  default = false
+}
+
+variable "jaeger_agent_endpoint" {
+  default = "localhost"
 }
--- traefik.tf
+++ traefik.tf
@@ -2,11 +2,13 @@ data "template_file" "traefik" {
   template = "${file("${path.module}/traefik.yaml.tmpl")}"
 
   vars {
-    prometheus_enabled = "${var.prometheus_enabled}"
-    ssl_enabled        = "${var.ssl_enabled}"
-    ssl_enforced       = "${var.ssl_enforced}"
-    ssl_cert_base64    = "${var.ssl_cert_base64}"
-    ssl_key_base64     = "${var.ssl_key_base64}"
+    jaeger_agent_endpoint = "${var.jaeger_agent_endpoint}"
+    prometheus_enabled    = "${var.prometheus_enabled}"
+    tracing_enabled       = "${var.tracing_enabled}"
+    ssl_enabled           = "${var.ssl_enabled}"
+    ssl_enforced          = "${var.ssl_enforced}"
+    ssl_cert_base64       = "${var.ssl_cert_base64}"
+    ssl_key_base64        = "${var.ssl_key_base64}"
   }
 }
 
@@ -31,6 +33,26 @@ resource "helm_release" "traefik" {
   set {
     name  = "replicas"
     value = "${var.ingress_replica_count}"
+  }
+
+  set {
+    name  = "tracing.backend"
+    value = "jaeger"
+  }
+
+  set {
+    name  = "tracing.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "tracing.jaeger.localAgentHostPort"
+    value = "jaeger-agent.jaeger.svc.cluster.local:6831"
+  }
+
+  set {
+    name  = "tracing.jaeger.samplingServerURL"
+    value = "http://jaeger-agent.jaeger.svc.cluster.local:5778/sampling}"
   }
 
   depends_on = ["null_resource.generate_traefik_config", "data.template_file.traefik"]
